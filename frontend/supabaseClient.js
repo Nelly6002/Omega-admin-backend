201ff6428c29cf36408df52cfaec4521a6669b89
MableTech <michaelonu39@gmail.com>
2025-11-07 18:44:16 +0100

    Co-authored-by: Hurleybaba <Hurleybaba@users.noreply.github.com>

--- frontend/supabaseClient.js
+++ frontend/supabaseClient.js
@@ -1,17 +1,13 @@
-// Supabase client setup// Supabase client setup
-
-const SUPABASE_URL = "https://nhptfcagmaphffiwjdxd.supabase.co";
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ocHRmY2FnbWFwaGZmaXdqZHhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyNDU5MjksImV4cCI6MjAxNDgyMTkyOX0.J7T76m4wJnGwT9_93KHNL5yF8ttHmS8cnOw7zLywUR4';
-
-
+const SUPABASE_URL = "https://gsbiwgnltvbpyexizmdo.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzYml3Z25sdHZicHlleGl6bWRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0MzMxNzUsImV4cCI6MjA3NTAwOTE3NX0.fGOzAbd3MYb27X_F1Zak7zV0EBt86fPwVKyVxr3k0Nk";
 
 // Initialize Supabase client with auto refresh token setting// Initialize Supabase client
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-
-    auth: {
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: true
-    }
+  auth: {
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
 });
